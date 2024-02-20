765f55400576e2e2e409f580bf657006726e392b
ishansssharma <143998034+ishansssharma@users.noreply.github.com>
2024-02-20 14:27:23 +0100

    create function for fething information

--- src/js/app.js
+++ src/js/app.js
@@ -21,3 +21,34 @@ overviewButtons.forEach(BBBButtons => {
 	}
 	BBBButtons.addEventListener("click", showList)
 })
+
+
+//Running the function when the buttons are clicked
+
+const fetchInfo = async () => {
+	const responseFilms = await fetch("https://swapi.dev/api/films/")
+	const result1 = await responseFilms.json();
+	console.log(result1.results);
+
+
+	const responsePeople = await fetch("https://swapi.dev/api/people/")
+	const result2 = await responsePeople.json();
+	console.log(result2.results);
+
+	const responsePlanets = await fetch("https://swapi.dev/api/planets/")
+	const result3 = await responsePlanets.json();
+	console.log(result3);
+
+	const responseVehicles = await fetch("https://swapi.dev/api/vehicles/")
+	const result4 = await responseVehicles.json()
+	console.log(result4.results);
+
+
+	renderFilms(result1.results);
+	renderPeople(result2.results);
+	renderPlanets(result3.results);
+	renderVehicles(result4.results);
+
+}
+
+fetchInfo()
